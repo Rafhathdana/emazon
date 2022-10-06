d1813b96000e65551b6d53d8c740c6679bf5c7ce
Rafhathdana <danarafha@gmail.com>
2022-10-06 19:12:07 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Emazon</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
