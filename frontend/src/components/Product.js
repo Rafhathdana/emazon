e84b0fd5ae933f131a56bff7725c00887194ee06
Rafhathdana <danarafha@gmail.com>
2022-10-13 00:33:52 +0530

    added bootstrap to features and make a component of product

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,21 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+function Product(props) {
+  const { product } = props;
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Card.Text>Rs {product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+export default Product;
