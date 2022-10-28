da2e9ef503b0db777937e2d0be4d850d84ca9a5a
Rafhathdana <danarafha@gmail.com>
2022-10-29 00:18:24 +0530

    created backend for product list

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const app = express();
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
+  }
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
